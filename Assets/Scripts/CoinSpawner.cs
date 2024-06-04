d721b9950f28c5df137f9a2d0abfe1e1422fbf61
Mustafa Kayabaş <mustafakayabas337@gmail.com>
2024-06-04 22:38:02 +0300

    Added CoinSpawner

---
+++ Assets/Scripts/CoinSpawner.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject coin;
+    [SerializeField] private GameObject leftWall, rightWall, upWall, botWall;
+    private int minX, maxX, minY, maxY;
+
+    private float SpawnCoinRate = 1f;
+
+    private void Awake()
+    {
+        minX = (int)leftWall.transform.localPosition.x + 26;
+        maxX = (int)rightWall.transform.localPosition.x - 26;
+        maxY = (int)upWall.transform.localPosition.y - 26;
+        minY = (int)botWall.transform.localPosition.y + 26;
+    }
+
+    public void Start()
+    {
+        StartCoroutine(SpawnCoin(SpawnCoinRate));    
+    }
+
+    private IEnumerator SpawnCoin(float time)
+    {
+        while (true)
+        {
+            if (Input.touchCount > 0)
+            {
+                while (true)
+                {
+                    yield return new WaitForSeconds(time);
+                    System.Random rand = new System.Random();
+                    float coinZ = -2;
+                    float coinY = rand.Next(maxY - minY) + minY;
+                    float coinX = rand.Next(maxX - minX) + minX;
+                    Vector3 position = new Vector3(coinX, coinY, coinZ);
+                    var obj = Instantiate(coin, position, Quaternion.identity, transform);
+                    obj.transform.localPosition = position;
+                    Destroy(obj, 2f);
+                }
+            }
+            else
+            {
+                yield return new WaitForSeconds(0.1f);
+            }
+        }
+    }
+}
